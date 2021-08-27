e56f608d95b4e53e5e459d4a69739712189d6f20
Pravin044 <pravin.pk044@gmail.com>
2021-08-27 19:05:07 +0530

    Add project files.

---
+++ CommandPromptWpf/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CommandPromptWpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Process process;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CmdConfig()
+        {
+            process = new Process();
+            process.StartInfo.FileName = "cmd.exe";
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.RedirectStandardInput = true;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.UseShellExecute = false;
+            process.Start();
+            process.OutputDataReceived += Process_OutputDataReceived;
+            process.BeginOutputReadLine();
+        }
+
+        private void ExicutetCommand(string command)
+        {
+            process.StandardInput.WriteLine(command);
+            process.StandardInput.Flush();
+        }
+
+        private void Process_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            Dispatcher.Invoke(() =>
+            {
+                cmdTerminal.AppendText(e.Data + "\n");
+            });
+
+        }
+
+        private void btnSendCommnad_Click(object sender, RoutedEventArgs e)
+        {
+            ExicutetCommand(txtCommand.Text);
+        }
+    }
+}
---
+++ KafakaClient/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+using Confluent.Kafka;
+namespace KafkaClient
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Process process;
+        private IConsumer<Ignore, string> consumer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+        }
+
+
+
+        private void btnStartConsume_Click(object sender, RoutedEventArgs e)
+        {
+            if (btnStartConsume.Content.Equals("Start Consume"))
+            {
+                Cursor = Cursors.Wait;
+                string uri = txtUrl.Text;
+                string topic = txtTopic.Text;
+                ConnectToKafakaConsume(uri, topic).ConfigureAwait(false);
+                btnStartConsume.Content = "Stop Consume";
+                Cursor = Cursors.Arrow;
+                txtTopic.IsEnabled = false;
+                txtUrl.IsEnabled = false;
+            }
+            else
+            {
+                txtTopic.IsEnabled = true;
+                txtUrl.IsEnabled = true;
+                consumer.Unsubscribe();
+                consumer.Close();
+                btnStartConsume.Content = "Start Consume";
+            }
+        }
+
+        private async Task ConnectToKafakaConsume(string uri, string topic)
+        {
+            var config = new ConsumerConfig
+            {
+                BrokerAddressFamily = BrokerAddressFamily.V4,
+                BootstrapServers = uri,
+                GroupId = "KafkaClient",
+
+            };
+            consumer = new ConsumerBuilder<Ignore, string>(config).Build();
+            CancellationToken cancellationToken = new CancellationToken();
+           await ConsumeData(cancellationToken, txtTopic.Text);
+
+        }
+
+        private async Task ConsumeData(CancellationToken cancellationToken, string topic)
+        {
+            try
+            {
+                consumer.Subscribe(topic);
+
+                var consumeResult = consumer.Consume(cancellationToken);
+                await Task.Factory.StartNew(MonitorData, consumeResult, cancellationToken).ConfigureAwait(false);
+            }
+            catch (Exception ex)
+            {
+                cmdTerminal.AppendText($"Error: {ex.Message}" + "\n");
+                cmdTerminal.ScrollToEnd();
+            }
+        }
+
+        async void MonitorData(object ObjData)
+        {
+            var data = (ConsumeResult<Ignore, string>)ObjData;
+            Dispatcher.Invoke(() =>
+            {
+                cmdTerminal.Foreground =Brushes.Red;
+                cmdTerminal.AppendText(data.Topic + " : " + data.Message.Value.ToString() + "\n");
+                cmdTerminal.ScrollToEnd();
+                cmdTerminal.Foreground = Brushes.White;
+            });
+        }
+
+
+    }
+}
